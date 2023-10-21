2653160649e029980bc422d8576e24e61996f7cd
Ramanath K P <ramnathnaik447@gmail.com>
2023-10-21 09:14:38 +0530

    add video details functionality on watch page

--- youtube-clone/src/components/WatchPage.jsx
+++ youtube-clone/src/components/WatchPage.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { DEFAULT_CHANNEL_LOGO, VIDEO_DETAILS_API } from "../utils/constants";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
+  const [videoDetails, setVideoDetails] = useState({});
   const videoId = searchParams.get("v");
 
+  const getVideoDetails = async () => {
+    const data = await fetch(
+      VIDEO_DETAILS_API +
+        videoId +
+        "&key=" +
+        process.env.REACT_APP_YOUTUBE_API_KEY
+    );
+    const json = await data.json();
+    const { title, description, channelTitle } = json?.items?.[0]?.snippet;
+    setVideoDetails({ title, description, channelTitle });
+  };
+
+  useEffect(() => {
+    getVideoDetails();
+  }, []);
+
   return (
-    <div className="m-2">
+    <div className="m-4 col-span-8">
       <iframe
         width="1000"
         height="500"
@@ -16,6 +34,20 @@ const WatchPage = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+      <div className="w-full m-2">
+        <h1 className="font-semibold text-xl my-2">{videoDetails.title}</h1>
+        <div className="flex items-center">
+          <img
+            className="w-12"
+            src={DEFAULT_CHANNEL_LOGO}
+            alt="default-channel-logo"
+          />
+          <p className="font-semibold my-2">{videoDetails.channelTitle}</p>
+        </div>
+        <div className="h-36 overflow-y-auto my-2 text-sm p-2 bg-slate-100 rounded-xl">
+          <p className="whitespace-pre-wrap">{videoDetails.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- youtube-clone/src/utils/constants.js
+++ youtube-clone/src/utils/constants.js
@@ -5,3 +5,7 @@ export const SEARCH_SUGGESTION_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const SEARCH_RESULTS_API =
   "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=";
+export const VIDEO_DETAILS_API =
+  "https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=";
+export const DEFAULT_CHANNEL_LOGO =
+  "https://static.vecteezy.com/system/resources/previews/023/986/480/original/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png";
